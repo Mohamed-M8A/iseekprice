b5b24c306b60ed449fcd84cadb64ecb0cf5683f0
Mohamed Adel <m7md20051968@gmail.com>
2026-05-13 08:05:06 +0300

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,47 @@
+const CACHE_NAME = 'iseekprice-v1';
+const PRE_CACHE_ASSETS = [
+  '/',
+  '/index.html',
+  '/public/assets/icons/192.png'
+];
+
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(PRE_CACHE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
 
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        }).catch(() => {
+          return cachedResponse; 
+        });
+
+        return cachedResponse || fetchedResponse;
+      });
+    })
+  );
 });
